6a84522f7bc835057c0e7f07b80502a396f15917
Artem Medvedev <artem_medvedev89@bk.ru>
2022-05-08 17:35:17 +0500

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,62 +1,95 @@
-// Исправьте функцию sendRequest
-// Аргументы функции:
-// - имя клиента
-// - телефон клиента
-// - объект с адресом доставки: {street, house, entrance, floor, flat}
-// - список товаров в заказе
-// - стоимость заказа с учетом скидок и доставки
-// Как результат функции требуется вернуть JSON,
-// cформированный в соответствии с правилами:
-// Объект data содержит все данные
-// В объекте data есть свойства:
-// - client - строка, имя клиента + телефон клиента;
-// - order - объект, содержащий данные о заказе:
-//     - address - строка с адресом доставки, записанным человекопонятным языком (как в примере)
-//     - sum - стоимость заказа с учетом скидок и доставки
-// - goods: массив объектов с информацией о позициях заказа:
-//     - title - название позиции
-//     - count - количество в заказе
-// например:
-// {
-//    "data": {
-//      "client": "Иван +7(987)65-43-210",
-//      "order": {
-//        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
-//        "sum": 900
-//       },
-//       "goods": [
-//         {
-//           "title": "Пицца",
-//           "count": 2
-//         }
-//      ]
-//    }
-// }
+
+
+constexpected = {
+    data: {
+        client: '',
+        order: {
+            address: '',
+            sum: 
+        },
+        goods: [
+            {
+                title: '',
+                count: 
+            }
+        ]
+    }
+};
+
+const name = '';
+        const phone = '';
+        constaddress= {
+            street: '',
+            house: '',
+            entrance: "",
+            floor: "",
+            flat: '',
+        };
+        const goods = [
+            {
+                id: ,
+                title: '',
+                count: ,
+                price: ''
+            }
+        ];
+        const sum = ;
+        const received = JSON.parse(sendRequest(name, phone, address, goods, sum));
+        expect(received).toEqual(expected);
+    });
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {
-        client: 'name + phone', 
-        order: {
-            address: 'street, house, entrance, floor, flat', 
-            sum: sum},
-        goods: [
-            {title: ,count:}
-        ],
-    }; 
-        
-
+    let data = {goods: [], order: {}};
+      
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
+    for (let i = 0; i < countOfGoods; i += 1) {
+        let goods = {title: "", count: 2};
         data.goods.push(goods[i].title);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван +7(987)65-43-210';
+    data.address = {
+        street: '',
+        house: '',
+        entrance: '',
+        floor: '',
+        flat: '',
+      };
+    data.sum = sum;
+ 
+    data.client = name + phone;
 
     let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 }
+
+const name = '';
+        const phone = '';
+        const address = {
+            street: '',
+            house: '',
+            entrance: '',
+            floor: '',
+            flat: '',
+        };
+        const goods = [
+            {
+                id: ,
+                title: '',
+                count: ,
+                price: ''
+            },
+            {
+                id: ,
+                title: '',
+                count: ,
+                price: ''
+            },
+        ];
+        const sum = ;
+
+        const received = JSON.parse(sendRequest(name, phone, address, goods, sum));
+        expect(received).toEqual(expected);
+    });
+})
