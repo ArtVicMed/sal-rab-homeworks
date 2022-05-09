8406530fd4ad09f5c272886b4d457262cb385377
Artem Medvedev <artem_medvedev89@bk.ru>
2022-05-09 12:36:05 +0500

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,42 +1,42 @@
 
 
-constexpected = {
-    data: {
-        client: '',
-        order: {
-            address: '',
-            sum: 
-        },
-        goods: [
-            {
-                title: '',
-                count: 
-            }
-        ]
-    }
-};
 
-const name = '';
-        const phone = '';
-        constaddress= {
-            street: '',
-            house: '',
-            entrance: "",
-            floor: "",
-            flat: '',
-        };
-        const goods = [
-            {
-                id: ,
-                title: '',
-                count: ,
-                price: ''
-            }
-        ];
-        const sum = ;
-        const received = JSON.parse(sendRequest(name, phone, address, goods, sum));
-        expect(received).toEqual(expected);
-    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function sendRequest(name, phone, address, goods, sum) {
     let data = {goods: [], order: {}};
@@ -44,52 +44,17 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i += 1) {
-        let goods = {title: "", count: 2};
-        data.goods.push(goods[i].title);
+        goods = {title: goods.title, count: goods.count};
+        data.goods.push(goods);
     }
 
-    data.address = {
-        street: '',
-        house: '',
-        entrance: '',
-        floor: '',
-        flat: '',
-      };
+    data.address = 'street: '',house: '',entrance: '',floor: '',flat: '', ';
+    
     data.sum = sum;
  
-    data.client = name + phone;
+    data.client = 'name + '' + phone';
 
     let jsonData = JSON.stringify({data: data});
 
     return jsonData;
-}
-
-const name = '';
-        const phone = '';
-        const address = {
-            street: '',
-            house: '',
-            entrance: '',
-            floor: '',
-            flat: '',
-        };
-        const goods = [
-            {
-                id: ,
-                title: '',
-                count: ,
-                price: ''
-            },
-            {
-                id: ,
-                title: '',
-                count: ,
-                price: ''
-            },
-        ];
-        const sum = ;
-
-        const received = JSON.parse(sendRequest(name, phone, address, goods, sum));
-        expect(received).toEqual(expected);
-    });
-})
+};
